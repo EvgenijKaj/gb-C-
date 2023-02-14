1d7bf2dfa40eb67848ef8bb32deaff3520d30289
Evgenij K/ <kaigorodov-1994@mail.ru>
2023-02-14 20:06:08 +0500

    added tasks and file gitignore

---
+++ Seminar01/task0/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+string? answer1 = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string? answer2 = Console.ReadLine();
+int number1 = Convert.ToInt32(answer1);
+int number2 = Convert.ToInt32(answer2);
+if (number1 == number2*number2)
+{
+    Console.WriteLine("Да, Первое число является квадратом второго");    
+}
+else 
+{
+    Console.WriteLine("Нет, Первое число НЕ является квадратом второго");
+}
+    
---
+++ Seminar01/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7, min = 5
+// a = 2 b = 10 -> max = 10, min = 2
+// a = -9 b = -3 -> max = -3, min = -9
+
+Console.Write("Введите первое число: ");
+string? answer1 = Console.ReadLine();
+int numberA = Convert.ToInt32(answer1);
+
+Console.Write("Введите второе число: ");
+string? answer2 = Console.ReadLine();
+int numberB = Convert.ToInt32(answer2);
+
+int min = 0;
+int max = 0;
+
+if (numberA > numberB)
+{
+    max = numberA;
+    min = numberB;
+}
+else
+{
+    min = numberA;
+    max = numberB;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max); 
+Console.Write("min = ");
+Console.WriteLine(min);
