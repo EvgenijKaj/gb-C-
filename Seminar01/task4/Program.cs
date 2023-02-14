11af0d86144471856f0ac566e62db641be6a9e2f
Evgenij K/ <kaigorodov-1994@mail.ru>
2023-02-14 20:59:09 +0500

    small additions

--- Seminar01/task4/Program.cs
+++ Seminar01/task4/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int n1 = 22;
+int n2 = 5;
+int n3 = 78;
+
+int max = 0;
+
+if ( n1 > max ) 
+{ 
+    max = n1;
+};
+
+if ( n2 > max ) 
+{ 
+    max = n1;
+};
+
+if ( n3 > max ) 
+{ 
+    max = n1;
+};
+
+Console.WriteLine(max);
