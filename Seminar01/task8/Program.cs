de6c50b9bd1244fd28c0cdd16de7e2a6e58f4df1
Evgenij K/ <kaigorodov-1994@mail.ru>
2023-02-14 20:56:49 +0500

    added task8, only even numbers

---
+++ Seminar01/task8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+string? answeruser = Console.ReadLine();
+int numb = Convert.ToInt32(answeruser);
+
+Console.WriteLine("Вывод чётных чисел от 1 ", numb); // numb почему-то не выводится
+for ( int i = 1; i <= numb; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
