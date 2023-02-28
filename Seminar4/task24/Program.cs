1d021357c2cf8e1e5593e84cdbd94bc6a659af02
Evgenij K/ <kaigorodov-1994@mail.ru>
2023-02-28 20:18:59 +0500

    task24 and small changes

---
+++ Seminar4/task24/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход 
+// число (А) и выдаёт сумму чисел от 1 до А.
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int SumToA(int a)
+{
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        sum = sum + i;
+        //sum +=i;
+    }
+    return sum;
+}
+
+int number = ReadNumber("Введите число А");
+int result = SumToA(number);
+Console.WriteLine(result);
--- Seminar4/task27/Program.cs
+++ Seminar4/task27/Program.cs
@@ -10,12 +10,10 @@ Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int sum = 0;
-while (number / 10 > 0)
+while (number > 0)
 {
     sum = sum + number % 10;
     number = number / 10;
 }
-
-sum = sum + number;
 
 Console.WriteLine(sum);
