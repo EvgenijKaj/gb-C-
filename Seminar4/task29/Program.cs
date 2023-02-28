7a0a1a160c82901ec730182f86c3da74f3a3271b
Evgenij K/ <kaigorodov-1994@mail.ru>
2023-02-28 19:39:03 +0500

    added tasks homework seminar 4

---
+++ Seminar4/task29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+
+Console.Write("[");
+for (int i = 0; i < 7; i++)
+{
+    array[i] = Random.Shared.Next(100);
+    Console.Write($"{array[i]}, ");
+}
+
+array[7] = Random.Shared.Next(100);
+Console.Write($"{array[7]}");
+Console.Write("]");
+    
