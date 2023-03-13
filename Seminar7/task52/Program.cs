895da25e569344c623a9906fdd90e50184dd83df
Evgenij K/ <kaigorodov-1994@mail.ru>
2023-03-13 21:32:21 +0500

    task52 fixed bugs

--- Seminar7/task52/Program.cs
+++ Seminar7/task52/Program.cs
@@ -43,15 +43,15 @@ void PrintMatrix(int[,] matrix)
 void AverageElementsColums(int[,] matrix)
 {
     double average = 0;
-    for(int i = 0; i < matrix.GetLength(0); i++)
+    for(int j = 0; j < matrix.GetLength(1); j++)
     {
         double sum = 0;
-        for(int j = 0; j < matrix.GetLength(1); j++)
+        for(int i = 0; i < matrix.GetLength(0); i++)
         {
-            sum += matrix[j, i];
+            sum += matrix[i, j];
         }
         average = sum/matrix.GetLength(0);
-        Console.WriteLine($"Среднее арифметическое {i} строки {average}; ");
+        Console.WriteLine($"Среднее арифметическое {j} столбца {average:f2}; ");
     }
 }
 
