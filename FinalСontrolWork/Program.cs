f8b8495cd5ff94c00cb67215e0cd2a9c7dbb1a8c
Evgenij K/ <kaigorodov-1994@mail.ru>
2023-03-28 19:57:22 +0500

    added method NewArray

--- FinalСontrolWork/Program.cs
+++ FinalСontrolWork/Program.cs
@@ -1,13 +1,32 @@
-﻿//Задача. Составить из имеющегося массива строк, массив со строками не более 3-ч символов.
+﻿//Задача. Составить из имеющегося массива строк, массив со строками не более 3-х символов.
 
-string[] GetRandomArray(int length)
+string[] GetConsoleArray(int length)
 {
     string[] array = new string[length];
 
     for(int i = 0; i < array.Length; i++)
     {
         Console.WriteLine($"Введите значение {i} элемента массива");
-        array[i] = Conole.ReadLine();
+        array[i] = Console.ReadLine();
     }
     return array;
 }
+
+void NewArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write($" {array[i]} ");
+        }
+    }
+    Console.Write("]");
+}
+
+
+
+
+
