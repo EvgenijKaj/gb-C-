3c36c8a00d19bf72c51d65786766a63f0210486b
Evgenij K/ <kaigorodov-1994@mail.ru>
2023-03-28 19:31:55 +0500

    add method GetConsoleArray

---
+++ FinalСontrolWork/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача. Составить из имеющегося массива строк, массив со строками не более 3-ч символов.
+
+string[] GetRandomArray(int length)
+{
+    string[] array = new string[length];
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите значение {i} элемента массива");
+        array[i] = Conole.ReadLine();
+    }
+    return array;
+}
